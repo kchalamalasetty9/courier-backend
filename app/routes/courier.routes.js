766de6441b35137cada5f2e9d4bbf0bc673627e8
Whoravigupta <ravi.gupta@eagles.oc.edu>
2023-07-06 14:11:04 -0500

    Courier

---
+++ app/routes/courier.routes.js
@@ -0,0 +1,13 @@
+module.exports = (app) => {
+  const courier = require("../controllers/courier.controller.js");
+
+  var router = require("express").Router();
+
+  router.post("/couriers", courier.create);
+  router.get("/couriers", courier.readAll);
+  router.get("/couriers/:id", courier.get);
+  router.put("/couriers/:id", courier.update);
+  router.delete("/couriers/:id", courier.delete);
+
+  app.use("/courierapi", router);
+};
