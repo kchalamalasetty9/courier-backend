b6258ddeef966e23bac4a929011ab67e9315cec3
Kinnera <k.chalamalasetty@eagles.oc.edu>
2023-07-06 16:22:16 -0500

    integration all the backend controller route and models.

--- app/models/index.js
+++ app/models/index.js
@@ -15,6 +15,11 @@ db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 db.session = require("./session.model.js")(sequelize, Sequelize);
 db.user = require("./user.model.js")(sequelize, Sequelize);
+db.ticket = require("./ticket.model.js")(sequelize,Sequelize);
+db.customer = require("./customer.model.js")(sequelize,Sequelize);
+db.courier = require("./courier.model.js")(sequelize,Sequelize);
+db.deliveryInfo = require("./deliveryInfo.model.js")(sequelize,Sequelize);
+db.clerk = require("./clerk.model.js")(sequelize,Sequelize);
 
 // foreign key for session
 db.user.hasMany(
@@ -22,10 +27,28 @@ db.user.hasMany(
   { as: "session" },
   { foreignKey: { allowNull: false }, onDelete: "CASCADE" }
 );
+
 db.session.belongsTo(
   db.user,
   { as: "user" },
   { foreignKey: { allowNull: false }, onDelete: "CASCADE" }
 );
+
+db.ticket.belongsTo(db.deliveryInfo, { foreignKey: 'deliveryInfoId' });
+db.deliveryInfo.hasOne(db.ticket, { foreignKey: 'deliveryInfoId' });
+
+db.ticket.belongsTo(db.customer, { foreignKey: 'orderedBy', as: 'customer' });
+db.customer.hasMany(db.ticket, { foreignKey: 'orderedBy', as: 'tickets' });
+
+db.ticket.belongsTo(db.courier, { foreignKey: 'courierNumber', as: 'courier' });
+db.courier.hasMany(db.ticket, { foreignKey: 'courierNumber', as: 'tickets' });
+
+db.clerk.belongsTo(db.user, { foreignKey: 'userId', as: 'user' });
+db.user.hasOne(db.clerk, { foreignKey: 'userId' });
+
+
+db.courier.belongsTo(db.user, { foreignKey: 'userId', as: 'user' });
+db.user.hasOne(db.courier, { foreignKey: 'userId' });
+
 
 module.exports = db;
