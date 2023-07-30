ca0969f176d0c1e3d881bd4abbb2d460034fb2fd
sairam <bheemreddy.sairam@eagles.oc.edu>
2023-07-30 16:24:43 -0500

    Order assign to courier

--- app/controllers/courier.controller.js
+++ app/controllers/courier.controller.js
@@ -8,7 +8,7 @@ exports.create = async (req, res) => {
     res.json(courier);
   } catch (err) {
     console.error(err);
-    res.status(500).json({ error: 'Failed to create courier' });
+    res.status(500).json({ error: "Failed to create courier" });
   }
 };
 
@@ -19,7 +19,7 @@ exports.readAll = async (req, res) => {
     res.json(couriers);
   } catch (err) {
     console.error(err);
-    res.status(500).json({ error: 'Failed to fetch couriers' });
+    res.status(500).json({ error: "Failed to fetch couriers" });
   }
 };
 
@@ -28,13 +28,13 @@ exports.get = async (req, res) => {
   try {
     const courier = await Courier.findByPk(req.params.id);
     if (!courier) {
-      res.status(404).json({ error: 'Courier not found' });
+      res.status(404).json({ error: "Courier not found" });
     } else {
       res.json(courier);
     }
   } catch (err) {
     console.error(err);
-    res.status(500).json({ error: 'Failed to fetch courier' });
+    res.status(500).json({ error: "Failed to fetch courier" });
   }
 };
 
@@ -43,14 +43,14 @@ exports.update = async (req, res) => {
   try {
     const courier = await Courier.findByPk(req.params.id);
     if (!courier) {
-      res.status(404).json({ error: 'Courier not found' });
+      res.status(404).json({ error: "Courier not found" });
     } else {
       await courier.update(req.body);
       res.json(courier);
     }
   } catch (err) {
     console.error(err);
-    res.status(500).json({ error: 'Failed to update courier' });
+    res.status(500).json({ error: "Failed to update courier" });
   }
 };
 
@@ -59,16 +59,41 @@ exports.delete = async (req, res) => {
   try {
     const courier = await Courier.findByPk(req.params.id);
     if (!courier) {
-      res.status(404).json({ error: 'Courier not found' });
+      res.status(404).json({ error: "Courier not found" });
     } else {
       await courier.destroy();
       db.user.destroy({
         where: { id: courier.userId },
-      })
-      res.json({ message: 'Courier deleted successfully' });
+      });
+      res.json({ message: "Courier deleted successfully" });
     }
   } catch (err) {
     console.error(err);
-    res.status(500).json({ error: 'Failed to delete courier' });
+    res.status(500).json({ error: "Failed to delete courier" });
+  }
+};
+
+exports.availableCourier = async (req, res) => {
+  try {
+    const occupiedCouriers = await db.courier.findAll({
+      include: [
+        {
+          model: db.ticket,
+          as: 'tickets',
+          where: { status: { [db.Sequelize.Op.eq]: 'pending' } }, // Filtering out couriers with status 'pending'
+          required: false,
+        },
+      ],
+      where: { '$tickets.ticketId$': { [db.Sequelize.Op.ne]: null } },
+    });
+
+    const couriers = await Courier.findAll({ where: null });
+    
+    const availableCouriers = couriers.filter(e => !occupiedCouriers.some(o => o.courierNumber === e.courierNumber))
+
+    res.json(availableCouriers);
+  } catch (error) {
+    console.error("Error fetching available couriers:", error);
+    res.status(500).json({ message: "Internal Server Error" });
   }
 };
