421e2c7c777ae7bb6cd4503cf40db2dc722d4338
Whoravigupta <ravi.gupta@eagles.oc.edu>
2023-07-31 14:41:51 -0500

    Company table changes

--- app/models/company.model.js
+++ app/models/company.model.js
@@ -1,10 +1,31 @@
 module.exports = (sequelize, Sequelize) => {
   const Company = sequelize.define('company', {
-    key: {
-      type: Sequelize.STRING,
+    id: {
+      type: Sequelize.INTEGER,
       primaryKey: true,
+      autoIncrement: true,
     },
-    value: {
+    name: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    price_per_block: {
+      type: Sequelize.FLOAT,
+      allowNull: false,
+    },
+    time_per_block: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+    },
+    ontime_bonus_percentage: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+    },
+    office_northsouth_street: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    office_eastwest_street: {
       type: Sequelize.STRING,
       allowNull: false,
     },
